e01d50f02ede17b4cda23c71b8925685af6f9a58
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-05 23:19:38 +0530

    Problem Solving Patterns: Multiple Pointers - Divide and Concoure : fix if confition

--- Problem-Solving-Patterns/6.divide-and-concure.go
+++ Problem-Solving-Patterns/6.divide-and-concure.go
@@ -6,9 +6,8 @@ import "fmt"
 func binarySearch(array []int, n int) int {
 	var min int = 0
 	var max int = len(array) - 1
-	var index int = -1
-	for min < max {
-		index = (min + max) / 2
+	for min <= max {
+		var index int = (min + max) / 2
 		if n == array[index] {
 			return index
 		}
@@ -18,10 +17,10 @@ func binarySearch(array []int, n int) int {
 			min = index + 1
 		}
 	}
-	return index
+	return -1
 }
 
 func main() {
 	var array = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(binarySearch(array, 3))
+	fmt.Println(binarySearch(array, 6))
 }
