f12b194e93ed9a233cef74240485e5c782de3d8d
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-11-13 11:52:49 +0530

    Problem Solving Patterns: sliding window

--- Problem-Solving-Patterns/sliding-window/max-subarray-sum.go
+++ Problem-Solving-Patterns/sliding-window/max-subarray-sum.go
@@ -1,6 +1,6 @@
 /**
  * Write a function called maxSubarraySum, which accepts an array of integers
- * and a number called n. The function should calculate the maximum sub of n
+ * and a number called n. The function should calculate the maximum sum of n
  * consecutive elements in array.
  */
 package main
